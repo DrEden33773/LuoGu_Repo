b11d9f7b1372b7bea5dc748efb2a2574b0a0b27e
DrEden33773 <edwardwang33773@gmail.com>
2022-10-25 14:25:26 +0800

    update

--- src/P_1002.hpp
+++ src/P_1002.hpp
@@ -14,63 +14,43 @@
 #pragma once
 
 #include <iostream>
-#include <unordered_set>
-#include <utility>
+#include <set>
 #include <vector>
 
-using namespace std;
-
 class P_1002 {
-    unordered_set<int> X_control;
-    unordered_set<int> Y_control;
-    pair<int, int>     destination;
-    pair<int, int>     horse_point;
+    std::set<std::pair<long long, long long>> control_point;
+    std::pair<long long, long long>           destination;
+    std::pair<long long, long long>           horse_point;
 
 public:
     void input() {
-        int des_X, des_Y;
-        int horse_X, horse_Y;
+        long long des_X, des_Y;
+        long long horse_X, horse_Y;
 
-        cin >> des_X >> des_Y >> horse_X >> horse_Y;
+        std::cin >> des_X >> des_Y >> horse_X >> horse_Y;
 
-        destination = make_pair(des_X, des_Y);
-        horse_point = make_pair(horse_X, horse_Y);
+        destination = std::make_pair(des_X, des_Y);
+        horse_point = std::make_pair(horse_X, horse_Y);
 
-        X_control.insert(horse_point.first);
-        Y_control.insert(horse_point.second);
-
-        X_control.insert(horse_point.first - 2);
-        Y_control.insert(horse_point.second + 1);
-
-        X_control.insert(horse_point.first - 1);
-        Y_control.insert(horse_point.second + 2);
-
-        X_control.insert(horse_point.first + 1);
-        Y_control.insert(horse_point.second + 2);
-
-        X_control.insert(horse_point.first + 2);
-        Y_control.insert(horse_point.second + 1);
-
-        X_control.insert(horse_point.first - 2);
-        Y_control.insert(horse_point.second - 1);
-
-        X_control.insert(horse_point.first - 1);
-        Y_control.insert(horse_point.second - 2);
-
-        X_control.insert(horse_point.first + 1);
-        Y_control.insert(horse_point.second - 2);
-
-        X_control.insert(horse_point.first + 2);
-        Y_control.insert(horse_point.second - 1);
+        auto& h = horse_point;
+        auto& c = control_point;
+        c.insert(h);
+        c.insert(std::make_pair(h.first - 2, h.second + 1));
+        c.insert(std::make_pair(h.first - 1, h.second + 2));
+        c.insert(std::make_pair(h.first + 1, h.second + 2));
+        c.insert(std::make_pair(h.first + 2, h.second + 1));
+        c.insert(std::make_pair(h.first - 2, h.second - 1));
+        c.insert(std::make_pair(h.first - 1, h.second - 2));
+        c.insert(std::make_pair(h.first + 1, h.second - 2));
+        c.insert(std::make_pair(h.first + 2, h.second - 1));
     }
-    int DP_FUNC(const int& x, const int& y) {
-        if (X_control.find(x) != X_control.end()
-            && Y_control.find(y) != Y_control.end()) {
+    long long DP_FUNC(const long long& x, const long long& y) {
+        if (control_point.find(std::make_pair(x, y)) != control_point.end()) {
             // find both coordinates!
-            return 0;
+            return 0LL;
         }
         if (!x && !y) {
-            return 1;
+            return 1LL;
         } else if (!x) {
             return DP_FUNC(x, y - 1);
         } else if (!y) {
@@ -81,7 +61,7 @@ public:
     void output() {
         auto&& d   = destination;
         auto&& res = DP_FUNC(destination.first, destination.second);
-        cout << res;
+        std::cout << res;
     }
     static void solution() {
         P_1002 TheSolution;
